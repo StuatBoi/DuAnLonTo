8b5c2df2cc959660735718c57c5b479f5a7f1260
Dang12e <vuquanhvinh123@gmail.com>
2026-05-25 22:46:46 +0700

    detail parse

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,6 @@ module org.net.demo {
     requires transitive javafx.graphics;
     
 
-    opens org.net.demo to javafx.fxml;
+    opens org.net.demo to javafx.fxml,com.google.gson;
     exports org.net.demo;
 }
