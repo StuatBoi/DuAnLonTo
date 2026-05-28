540263731d2d9d566808b5c2b4857139b6591c71
Hoang <hoanganhle121106@gmail.com>
2026-05-28 15:28:56 +0700

    sua nhe

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,8 @@ module org.net.demo {
     requires javafx.fxml;
     requires com.google.gson;
     requires transitive javafx.graphics;
-
+    requires jdk.httpserver;
+    requires java.net.http;
     opens org.net.demo to javafx.fxml,com.google.gson;
 
     exports org.net.demo;
--- src/main/java/org/net/demo/MovieService.java
+++ src/main/java/org/net/demo/MovieService.java
@@ -3,8 +3,14 @@ package org.net.demo;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 
 public class MovieService {
@@ -14,18 +20,28 @@ public class MovieService {
     public static void loadMovies() {
 
         try {
-            System.out.println(
-                    MovieService.class.getResourceAsStream(
-                            "/org/net/demo/info.json"
-                    )
-            );
-            InputStreamReader reader = new InputStreamReader(MovieService.class.getResourceAsStream("/org/net/demo/info.json"));
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request =
+                    HttpRequest.newBuilder()
+                            .uri(URI.create("http://localhost:8080/movies"))
+                            .build();
+
+            HttpResponse<String> response =
+                    client.send(request,
+                            HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+
+            System.out.println(json);
 
             Gson gson = new Gson();
 
-            Type movieListType = new TypeToken<ArrayList<Movie>>(){}.getType();
+            Type type =
+                    new TypeToken<ArrayList<Movie>>(){}.getType();
 
-            movies = gson.fromJson(reader, movieListType);
+            movies = gson.fromJson(json, type);
+
             System.out.println(movies.size());
 
         } catch (Exception e) {
